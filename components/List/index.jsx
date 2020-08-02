a00178464b6d15b00a6e4590e8e42a6e3bf71df5
Ali Almohaya <alialmohaya@gmail.com>
2020-08-02 19:54:51 +0200

    chore: add dummy data to the list tab

--- components/List/index.jsx
+++ components/List/index.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import styles from './List.module.scss'
 import { useTranslation } from '../../i18n'
 import Topbar from '../Topbar'
+import AnimeList from '../AnimeList'
 
 export default function List() {
   const { t } = useTranslation()
@@ -10,6 +11,7 @@ export default function List() {
   return (
     <main className={styles.main}>
       <Topbar heading={t('animes-list')} />
+      <AnimeList />
     </main>
   )
 }
