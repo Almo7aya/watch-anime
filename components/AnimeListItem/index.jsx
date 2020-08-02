0c8aea474cf06451bf184fcfac4e59a71da0d9cc
Ali Almohaya <alialmohaya@gmail.com>
2020-08-02 14:49:14 +0200

    fix: translates

--- components/AnimeListItem/index.jsx
+++ components/AnimeListItem/index.jsx
@@ -2,22 +2,27 @@ import React from 'react'
 import propTypes from 'prop-types'
 
 import styles from './AnimeListItem.module.scss'
+import { useTranslation } from '../../i18n'
 
 export default function AnimeListItem({ animeData, showLastEpisode = true }) {
+  const { t } = useTranslation()
+
   return (
     <div className={styles.animelistitem}>
 
       <div className={styles.floating_details}>
         <img className={styles.floating_details_img} src={animeData.anime_cover_image_url} />
         <p className={styles.floating_details_rating}>{animeData.anime_rating}</p>
-        <p className={styles.floating_details_last_episode_name}>{animeData.latest_episode_name}</p>
+        {
+          showLastEpisode && <p className={styles.floating_details_last_episode_name}>{animeData.latest_episode_name}</p>
+        }
       </div>
 
       <div className={styles.static_details}>
         <p className={styles.static_details_title}>{animeData.anime_name}</p>
         <div className={styles.static_details_bottom_row}>
           <p>{animeData.anime_release_year}</p>
-          <p>{animeData.anime_season}</p>
+          <p>{animeData.anime_season && t(animeData.anime_season)}</p>
         </div>
       </div>
     </div>
