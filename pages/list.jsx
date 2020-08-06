b500b4d0bea81e8cba7df6227c6af462f5a085fa
Ali Almohaya <alialmohaya@gmail.com>
2020-08-06 23:48:43 +0200

    fix: list page data fetching

--- pages/list.jsx
+++ pages/list.jsx
@@ -1,20 +1,45 @@
 import React from 'react'
+import propTypes from 'prop-types'
 
 import { useTranslation } from '../i18n'
 import Topbar from '../components/Topbar'
 import AnimeList from '../components/AnimeList'
+import httpClient from '../utils/http-client'
 
-export default function ListPage() {
+export default function ListPage({ data, status }) {
   const { t } = useTranslation()
+
+  if (!data || status < 200) return <h1>SERVER IS OUT!</h1>
+
+  const { response: { data: animesData } } = data
 
   return (
     <>
       <Topbar heading={t('animes-list')} />
-      <AnimeList />
+      <AnimeList animesData={animesData} />
     </>
   )
 }
 
-ListPage.getInitialProps = async () => ({
-  namespacesRequired: ['common']
-})
+ListPage.getInitialProps = async () => {
+  let res
+  try {
+    res = await httpClient.get('list')
+  } catch (e) {
+    res = {
+      data: null,
+      status: 500
+    }
+  }
+
+  return {
+    namespacesRequired: ['common'],
+    data: res.data,
+    status: res.status
+  }
+}
+
+ListPage.propTypes = {
+  data: propTypes.object.isRequired,
+  status: propTypes.number.isRequired
+}
