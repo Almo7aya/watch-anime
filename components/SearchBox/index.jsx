201e49d28d5f0147c183224045335785ba8b3a57
Ali Almohaya <alialmohaya@gmail.com>
2020-08-06 20:55:44 +0200

    fix: search box position

--- components/SearchBox/index.jsx
+++ components/SearchBox/index.jsx
@@ -3,11 +3,12 @@ import propTypes from 'prop-types'
 import { useRouter } from 'next/router'
 
 import styles from './SearchBox.module.scss'
+import { useTranslation } from '../../i18n'
 
 export default function SearchBox({ initialSearchValue = '', onValueChange }) {
   const router = useRouter()
   const [searchValue, setSearchValue] = useState(initialSearchValue)
-
+  const { t } = useTranslation()
   useEffect(() => {
     // to change the pathname value dynamically
     router.push(`/search/[[...query]]?query=${searchValue}`, `/search/${searchValue}`, { shallow: true })
@@ -21,7 +22,7 @@ export default function SearchBox({ initialSearchValue = '', onValueChange }) {
   return (
     <input className={styles.searchbox} onChange={(e) => {
       setSearchValue(e.target.value)
-    }} value={searchValue} />
+    }} value={searchValue} placeholder={t('search')} />
   )
 }
 
