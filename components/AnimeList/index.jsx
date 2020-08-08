255329dd55810daf59fe6528159a4df2aab78236
Ali Almohaya <alialmohaya@gmail.com>
2020-08-08 20:02:58 +0200

    chore: add infinite loading to the animelist component

--- components/AnimeList/index.jsx
+++ components/AnimeList/index.jsx
@@ -1,21 +1,38 @@
 import React from 'react'
 import propTypes from 'prop-types'
+import useInfiniteScroll from '../../hooks/useInfiniteScroll'
 
 import styles from './AnimeList.module.scss'
 import AnimeListItem from '../AnimeListItem'
+import withInfiniteLoading from '../withInfiniteLoading'
 
-export default function AnimeList({ animesData }) {
+const AnimeList = ({ animesData, isLoading, loadMore }) => {
+  const infiniteRef = useInfiniteScroll({
+    onLoadMore: loadMore,
+    hasNextPage: true,
+    loading: isLoading
+  })
+
   return (
-    <section className={styles.animelist}>
+    <>
+      <section ref={infiniteRef} className={styles.animelist}>
+        {
+          animesData.map((anime) => {
+            return (<AnimeListItem animeData={anime} key={anime.anime_id} />)
+          })
+        }
+      </section>
       {
-        animesData.map((anime) => {
-          return (<AnimeListItem animeData={anime} key={anime.anime_id} />)
-        })
+        isLoading ? <p>Loading...</p> : <button onClick={loadMore}>Loadmore</button>
       }
-    </section>
+    </>
   )
 }
 
 AnimeList.propTypes = {
-  animesData: propTypes.array.isRequired
+  animesData: propTypes.array.isRequired,
+  isLoading: propTypes.bool.isRequired,
+  loadMore: propTypes.func.isRequired
 }
+
+export default withInfiniteLoading(AnimeList)
