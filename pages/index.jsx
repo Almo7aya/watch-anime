065c477c0b4683b1810e8cb95b7a58e00d88194b
Ali Almohaya <alialmohaya@gmail.com>
2020-08-08 20:03:30 +0200

    chore: add infinite logic for the homepage component

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,18 +5,21 @@ import { useTranslation } from '../i18n'
 import Topbar from '../components/Topbar'
 import AnimeList from '../components/AnimeList'
 import httpClient from '../utils/http-client'
+import config from '../config.json'
+
+const ENDPOINT = 'homepage'
 
 export default function IndexPage({ data, status }) {
   const { t } = useTranslation()
 
   if (!data || status < 200) return <h1>SERVER IS OUT!</h1>
 
-  const { response: { data: animesData } } = data
+  const { response } = data
 
   return (
     <>
       <Topbar heading={t('latest-animes')} />
-      <AnimeList animesData={animesData} />
+      <AnimeList initailAnimeData={response.data} endpoint={ENDPOINT} />
     </>
   )
 }
@@ -24,7 +27,11 @@ export default function IndexPage({ data, status }) {
 IndexPage.getInitialProps = async () => {
   let res
   try {
-    res = await httpClient.get('homepage')
+    res = await httpClient.get(ENDPOINT, {
+      params: {
+        _limit: config.initialPageLimit
+      }
+    })
   } catch (e) {
     res = {
       data: null,
