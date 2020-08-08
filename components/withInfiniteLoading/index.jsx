3c746fd49ff066a1c47e7978993e6ab49bf69991
Ali Almohaya <alialmohaya@gmail.com>
2020-08-08 20:01:53 +0200

    chore: add withIniniteLoadin HOC

---
+++ components/withInfiniteLoading/index.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react'
+import propsTypes from 'prop-types'
+
+import httpClient from '../../utils/http-client'
+import config from '../../config.json'
+
+export default function withInfiniteLoading(WrappedComponent) {
+  const infiniteLoading = ({ initailAnimeData, endpoint = '', disableInfinite = false }) => {
+    const [offset, setOffset] = useState(config.initialPageLimit)
+    const [animesData, setAnimesData] = useState(initailAnimeData)
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+      if (animesData.length === offset || disableInfinite) return // dont getData if the size is equal
+      setIsLoading(true)
+
+      httpClient.get(endpoint, {
+        params: {
+          _offset: offset,
+          _limit: config.lazyLoadLimit
+        }
+      }).then(({ data }) => {
+        setIsLoading(false)
+        const newData = data.response.data
+        setAnimesData([...animesData, ...newData])
+      }).catch((e) => {
+        setError(e)
+        setIsLoading(false)
+      })
+    }, [offset])
+
+    return (<>
+      <WrappedComponent animesData={animesData} isLoading={isLoading} loadMore={() => setOffset(offset + config.lazyLoadLimit)} error={error} />
+    </>)
+  }
+
+  propsTypes.propsTypes = {
+    initailAnimeData: propsTypes.array.isRequired,
+    endpoint: propsTypes.string,
+    disableInfinite: propsTypes.bool
+  }
+
+  return infiniteLoading
+}
