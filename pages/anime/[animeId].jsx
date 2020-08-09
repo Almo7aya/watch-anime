ef165da21a4e7bf2857d63a2ac48709a4c7bcb05
Ali Almohaya <alialmohaya@gmail.com>
2020-08-09 14:59:02 +0200

    chore: add animeId page

---
+++ pages/anime/[animeId].jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import propTypes from 'prop-types'
+
+import { useTranslation } from '../../i18n'
+import Topbar from '../../components/Topbar'
+import AnimeDetails from '../../components/AnimeDetails'
+import httpClient from '../../utils/http-client'
+
+const ENDPOINT = 'anime-details'
+
+export default function AnimePage({ animeId, data, status }) {
+  const { t } = useTranslation()
+
+  if (!data || status < 200) return <h1>SERVER IS OUT!</h1>
+
+  const { response } = data
+
+  return (
+    <>
+      <Topbar heading={t('anime')} />
+      <AnimeDetails data={response} />
+    </>
+  )
+}
+
+AnimePage.getInitialProps = async ({ query }) => {
+  const { animeId } = query
+  let res
+  try {
+    res = await httpClient.get(ENDPOINT, {
+      params: {
+        anime_id: animeId,
+        fetch_episodes: 'No' // TODO: check this later
+      }
+    })
+  } catch (e) {
+    res = {
+      data: null,
+      status: 500
+    }
+  }
+
+  return {
+    namespacesRequired: ['common'],
+    animeId,
+    data: res.data,
+    status: res.status
+  }
+}
+
+AnimePage.propTypes = {
+  data: propTypes.object.isRequired,
+  status: propTypes.number.isRequired,
+  animeId: propTypes.string
+}
